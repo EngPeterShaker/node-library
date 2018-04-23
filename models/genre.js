bc5dfe29609b8022656c80067b72743dc3bfc446
petershaker <peters.shaker@gmail.com>
2018-04-23 18:04:58 +0200

    models

---
+++ models/genre.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var BookGenreSchema = new Schema(
+    {
+        name: { type: String, required: true, min: 3, max: 100 }
+    }
+);
+
+BookGenreSchema
+    .virtual('url')
+    .get(function () {
+        return '/catalog/bookgenre/' + this._id;
+    });
+
+module.exports = mongoose.model('BookGenre', BookGenreSchema);
+    
